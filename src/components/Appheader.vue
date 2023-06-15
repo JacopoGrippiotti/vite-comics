3563a93d4617654b00f09db9599f5efe147ddf2d
JacopoGrippiotti <jacopo.grippiotti@gmail.com>
2023-06-15 17:32:41 +0200

    added components to App.vue

--- src/components/Appheader.vue
+++ src/components/Appheader.vue
@@ -4,6 +4,7 @@
 
 <script>
 export default {
+    name: 'AppHeader',
     data() {
         return {
 
@@ -13,6 +14,6 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-@use '../styles/partials/mixins' as*;
-@use '../styles/partials/variables' as*;
+  @use '../styles/partials/mixins' as*;
+  @use '../styles/partials/variables' as*;
 </style>
