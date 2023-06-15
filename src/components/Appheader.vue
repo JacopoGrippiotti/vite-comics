b07b047721613d40fc85943fcf8255f60490dfd1
JacopoGrippiotti <jacopo.grippiotti@gmail.com>
2023-06-15 18:46:40 +0200

    styled and structured AppHeader component

--- src/components/Appheader.vue
+++ src/components/Appheader.vue
@@ -1,5 +1,16 @@
 <template>
-    <h1>ciao</h1>
+    <div class="container">
+        <div class="logo-img" >
+            <img :src='logoImage.src' alt="">
+        </div>
+        <ul class="nav-bar">
+            <li v-for="item in navItems">
+                <a href="#">
+                    {{item}}
+                </a>
+            </li>
+        </ul>
+    </div>
 </template>
 
 <script>
@@ -7,7 +18,21 @@ export default {
     name: 'AppHeader',
     data() {
         return {
-
+            logoImage:{
+                src:"../../src/assets/img/dc-logo.png"
+            },
+            navItems:[
+                'characters',
+                'comics',
+                'movies',
+                'tv',
+                'games',
+                'collectibles',
+                'videos',
+                'fans',
+                'news',
+                'shop'
+        ]
         }
     },
 }
@@ -16,4 +41,21 @@ export default {
 <style lang="scss" scoped>
   @use '../styles/partials/mixins' as*;
   @use '../styles/partials/variables' as*;
+
+  div.container{
+    @include flex(row,space-between,center,nowrap);
+    margin: 20px auto;
+    ul{
+        @include flex(row);
+        font-size: 1.1rem;
+        
+        li{
+            margin: 5px;
+        }
+    }
+    img{
+        height: 70px;
+    }
+
+  }
 </style>
