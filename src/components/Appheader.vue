836188f44a251ec78bd9e589c0c07acc50c1bf8c
JacopoGrippiotti <jacopo.grippiotti@gmail.com>
2023-06-15 16:41:23 +0200

    added content to AppHeader component

---
+++ src/components/Appheader.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+
+        }
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+@use '../styles/partials/mixins' as*;
+@use '../styles/partials/variables' as*;
+</style>
