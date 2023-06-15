3fd51b7b40fe22bcb185120e51edc9963bb460ba
JacopoGrippiotti <jacopo.grippiotti@gmail.com>
2023-06-15 17:11:33 +0200

    added AppFooter component to project

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+
+    </div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+
+        }
+    },
+}
+</script>
+<style lang="scss">
+   @use '../styles/partials/mixins' as*;
+   @use '../styles/partials/variables' as*;
+</style>
