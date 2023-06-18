a905674d018fdb1e678f2bcfb658285fed7b4511
JacopoGrippiotti <jacopo.grippiotti@gmail.com>
2023-06-18 21:05:34 +0200

    structured props to populate AppMain component

---
+++ src/components/FilmItem.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="image-container">
+        <img :src="filmElement.thumb" alt="">
+    </div>
+</template>
+
+<script>
+export default {
+    name:'FilmItem',
+    props: {
+        filmElement : Object
+    },
+    
+}
+</script>
+<style lang="scss" scoped>
+    div.image-container{
+        width: calc((100% / 6) - 50px);
+        height: 100px;
+        img{
+            height: 100%;
+        }
+    }
+</style>
