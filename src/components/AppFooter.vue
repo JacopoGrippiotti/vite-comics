4fbea60ccd328ab459634cde5ecf585d5cdafa85
JacopoGrippiotti <jacopo.grippiotti@gmail.com>
2023-06-18 18:36:32 +0200

    styled AppFooter component and added list dinamically from data in list-items

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -2,8 +2,14 @@
     <footer>
         <div class="footer-top">
             <div class="container footer-jumbo-top" >
+                    <div class="list-container" v-for="(item,index) in listItems">
+                        <h1>{{ item.title }}</h1>
+                        <ul>
+                            <li v-for="item in listItems[index].items" >{{ item }}</li>
+                        </ul>
+                    </div>
 
-                <div class="background-container"></div>
+                      <div class="background-container"></div>
 
             </div>
         </div>
@@ -22,7 +28,46 @@ export default {
     name: 'AppFooter',
     data() {
         return {
+            listItems:[
+                {
+                    title:"Dc comics",
+                    items:["Characters",
+                           "Comics",   
+                           "Movies",
+                           "TV",
+                           "Games",
+                           "Videos",
+                           "News"]
+                },
+                {
+                    title:"Shop",
+                    items:["Shop DC",
+                           "Shop DC Collectibles"]
+                },
+                {
+                    title:"DC",
+                    items:["Terms Of Use",
+                           "Privacy policy (New)" ,  
+                           "Ad Choices" ,  
+                           "Advertising" ,  
+                           "Jobs" ,  
+                           "Subscriptions" ,  
+                           "Talent Workshops" ,  
+                           "CPSC Certificates" ,  
+                           "Ratings" ,  
+                           "Shop Help" ,  
+                           "Contact Us" ]
+                },
+                {
+                    title:"Sites",
+                    items:["DC",
+                           "Mad Magazine",
+                           "DC Kids",
+                           "DC Universe",
+                           "DC Power Visa"]
+                }
 
+            ]
         }
     },
 }
@@ -45,9 +90,28 @@ export default {
 
             div.footer-jumbo-top{
                 position: relative;
-                width: 100%;
                 height: 100%;
+                display: flex;
+                flex-direction: column;
+                flex-wrap: wrap;
+                margin: 0 auto;
+                align-content: flex-start;
 
+                div.list-container{
+                    width: 15%;
+                    margin-top: 30px;
+                    color: white;
+
+                    h1{
+                        font-size: 1.3rem;
+                        font-weight: 700;
+                    }
+
+                    li{
+                        margin-top: 4px;
+                        font-size: 0.85rem;
+                    }
+                }
                 div.background-container{
                     position: absolute;
                     background-image: url(../assets/img/dc-logo-bg.png);
